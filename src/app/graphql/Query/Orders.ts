dec54fe8106468d78aed3874bfbdab3affe10c9b
To'yliyev.Elyor <tuyliyevelyor@gmail.com>
2024-11-22 18:39:46 +0500

    add query get orders

--- src/app/graphql/Query/Orders.ts
+++ src/app/graphql/Query/Orders.ts
@@ -43,3 +43,47 @@ export const GET_ORDER_BY_ID = gql`
     }
   }
 `;
+
+export const GET_ORDERS = gql(`
+  query getOrders($statuses: String, $page: Int, $limit: Int) {
+      getOrders(statuses: $statuses, page: $page, limit: $limit) {
+        payload {
+            _id
+            totalPrice
+            createdAt
+            status
+            to
+            foods {
+                _id
+                quantity
+                price
+                discount
+                user
+                food {
+                    _id
+                    shortName
+                    name
+                    image
+                    description
+                    price
+                    discount
+                    likes
+                    category {
+                   _id
+                   name
+                   image
+                    }
+                }
+            }
+            createdBy {
+                _id
+                name
+                phone
+                role
+                photo
+                telegramId
+            }
+        }
+      }
+  }
+`);
--- src/app/pages/Order/index.tsx
+++ src/app/pages/Order/index.tsx
@@ -2,57 +2,19 @@ import { Box } from "@mui/material";
 import OrderStyle from "./Order.style";
 import OrderTable from "./components/OrderTable";
 import { OrderType } from "../../types/order";
-
-const orders: OrderType[] = [
-  {
-    id: 1,
-    status: "pending",
-    totalPrice: 47835435,
-    foods: [
-      {
-        id: 1,
-        title: "Burger",
-        img: "httpsL//dfa;fkd;.png",
-        price: 20000,
-        quantity: 2,
-      },
-      {
-        id: 2,
-        title: "Burger",
-        img: "httpsL//dfa;fkd;.png",
-        price: 20000,
-        quantity: 2,
-      },
-    ],
-  },
-  {
-    id: 2,
-    status: "go",
-    totalPrice: 100000,
-    foods: [
-      {
-        id: 1,
-        title: "Burger",
-        img: "httpsL//dfa;fkd;.png",
-        price: 20000,
-        quantity: 2,
-      },
-      {
-        id: 2,
-        title: "Burger",
-        img: "httpsL//dfa;fkd;.png",
-        price: 20000,
-        quantity: 2,
-      },
-    ],
-  },
-];
+import { useQuery } from "@apollo/client";
+import { GET_ORDERS } from "../../graphql/Query/Orders";
 
 const Order = () => {
+  const { loading, data, error } = useQuery(GET_ORDERS, {
+    variables: { statuses: "", page: 1, limit: 10 },
+  });
+
+  const orders = data?.getOrders?.payload || [];
   return (
     <OrderStyle>
       <Box className="order__wrapper">
-        {orders.map((item) => (
+        {orders.map((item: any) => (
           <OrderTable {...item} />
         ))}
       </Box>
